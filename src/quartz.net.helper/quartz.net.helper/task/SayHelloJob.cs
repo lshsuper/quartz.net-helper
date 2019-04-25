b371d185b10f52ef5cbb70f02c7d731a3009d2c4
liushuai <1308956271@qq.com>
2019-04-25 20:37:49 +0800

    【commit  取出敏感】

--- src/quartz.net.helper/quartz.net.helper/task/SayHelloJob.cs
+++ src/quartz.net.helper/quartz.net.helper/task/SayHelloJob.cs
@@ -1,8 +1,4 @@
-﻿using Aliyun.Acs.Core;
-using Aliyun.Acs.Core.Exceptions;
-using Aliyun.Acs.Core.Profile;
-using Aliyun.Acs.Dm.Model.V20151123;
-using NLog;
+﻿using NLog;
 using Quartz;
 using System;
 using System.Collections.Generic;
@@ -23,37 +19,6 @@ namespace quartz.net.helper.task
         public Task Execute(IJobExecutionContext context)
         {
 
-            IClientProfile profile = DefaultProfile.GetProfile("cn-hangzhou", "LTAINKJGWLmATlqd", "X7hVz2i0MZfh1SpUtERomAUXfzLudf");
-            IAcsClient client = new DefaultAcsClient(profile);
-            SingleSendMailRequest request = new SingleSendMailRequest();
-            try
-            {
-                //Version must set to "2017-06-22" when the regionId is not "cn-hangzhou"
-                //request.Version = "2017-06-22";
-                request.AccountName = "lshsuper@api.lshblog.club";
-                request.FromAlias = "lsh";
-                request.AddressType = 1;
-                request.TagName = "控制台创建的标签";
-                request.ReplyToAddress = true;
-                request.ToAddress = "1308956271@qq.com";
-                request.Subject = "默认标题";
-                request.HtmlBody = "默认主题"+ context.Trigger.JobKey;
-
-                SingleSendMailResponse httpResponse = client.GetAcsResponse(request);
-
-            }
-            catch (System.Runtime.Remoting.ServerException e)
-            {
-
-            }
-            catch (ClientException e)
-            {
-
-            }
-            catch (Exception e)
-            {
-
-            }
             Console.WriteLine(context.Trigger.JobKey);
             return Task.FromResult(0);
         }
